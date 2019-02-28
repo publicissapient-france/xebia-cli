1cbcd443c9c9ddc7c558efec9a8041c4eaee1ca5
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-02-28 18:47:04 +0100

    Echoes collection: add custom serializer to pretty-print in console

--- src/collections.rs
+++ src/collections.rs
@@ -1,7 +1,21 @@
+use std::fmt;
 use echo;
 
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize)]
 #[serde(untagged)]
 pub enum Echoes {
     Object(Vec<echo::Echo>)
+}
+
+impl fmt::Debug for Echoes {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Echoes::Object(echoes) => {
+                for echo in echoes {
+                    write!(f, "{:?}\n", echo);
+                }
+                return write!(f, "\n");
+            },
+        }
+    }
 }
