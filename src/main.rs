002c8a66128e42c7551cfbc772a0f828d7259867
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-02-28 16:48:05 +0100

    Handle CLI options and echoes subcommand with clap

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,14 @@ impl RestPath<()> for collections::Echoes {
     fn get_path(_: ()) -> Result<String, restson::Error> { Ok(String::from("echoes")) }
 }
 
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
+    let yaml = load_yaml!("../cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
     //let mut client = RestClient::new("https://beta.todoist.com").unwrap();
     let mut client = RestClient::new("https://api-dev.xebia.fr").unwrap();
 
@@ -25,23 +32,25 @@ fn main() {
     let api_key = env::var("XDD_API_KEY").unwrap();
     client.set_header("Authorization", &format!("Bearer {}", api_key)).unwrap();
 
-    let answer: Result<collections::Echoes, restson::Error> = client.get(());
-    match answer {
-        Ok(echoes)  => {
-            // We need to extract the enum value
-            match echoes {
-                collections::Echoes::Object(echoes) => {
-                    println!("{:?}", echoes);
+    if let Some(matches) = matches.subcommand_matches("echoes") {
+        let answer: Result<collections::Echoes, restson::Error> = client.get(());
+        match answer {
+            Ok(echoes)  => {
+                // We need to extract the enum value
+                match echoes {
+                    collections::Echoes::Object(echoes) => {
+                        println!("{:?}", echoes);
 
-                    println!{"== Tasks =="};
+                        println!{"== Tasks =="};
 //                    println!("Total: {}", tasks_metrics.total);
 //                    println!("Completed: {}", tasks_metrics.completed);
 //                    println!("To-do: {}", tasks_metrics.todo);
 //                    println!("Total P1: {}", tasks_metrics.total_p1);
+                    }
                 }
-            }
-        },
-        Err(e) => println!("Error: {:?}", e),
+            },
+            Err(e) => println!("Error: {:?}", e),
+        }
     }
 }
 
