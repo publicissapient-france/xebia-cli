fd89f31bb124250a6df1d392a3ebd1f673f7a13a
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-02-28 18:46:42 +0100

    Echo: add custom formatter to pretty-print in console

--- src/echo.rs
+++ src/echo.rs
@@ -1,7 +1,14 @@
-#[derive(Deserialize,Debug)]
+use std::fmt;
+
+#[derive(Deserialize)]
 pub struct Echo {
     pub edition: u32,
     pub title: String,
     pub author: String,
 }
 
+impl fmt::Debug for Echo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}. {} ({})", self.edition, self.title, self.author)
+    }
+}
