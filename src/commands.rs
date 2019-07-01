01696d1b65e67dc3fc2dfa1e9e6c64aabdb0c8a2
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-07-01 16:04:33 +0200

    Commands: add login command processing function

--- src/commands.rs
+++ src/commands.rs
@@ -4,6 +4,7 @@ use restson::RestPath;
 use settings;
 use stats::EchoesStats;
 use xdd_api_client;
+use auth;
 
 impl RestPath<()> for Echoes {
     fn get_path(_: ()) -> Result<String, restson::Error> {
@@ -61,4 +62,9 @@ pub fn process_meti_command(command: &ArgMatches, settings: settings::Settings) 
             Err(e) => println!("Error: {:?}", e),
         }
     }
+}
+
+pub fn process_login_command(command: &ArgMatches, settings: settings::Settings) {
+    log::info!("Logging in...");
+    auth::auth();
 }
